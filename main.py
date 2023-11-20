37c0835cbf07cf979b523925c48721f705504c5e
Dmitry Galkin <d.galkin.89@gmail.com>
2023-11-20 22:52:58 +0300

    update requirements.txt

--- main.py
+++ main.py
@@ -3,17 +3,19 @@
 from src.simulation import Simulation
 
 # Время моделирования, с.
-SIMULATION_TIME = 86400
+SIMULATION_TIME = 60
 # Шаг моделирования, с.
 TIME_STEP = 60
 # Учет возмущений.
 USE_PERTURBATION = True
+# Имя файла, куда записываются результаты (лежит в папке results).
+FILENAME_WITH_RESULTS = "default.txt"
 
 if __name__ == "__main__":
 
     print("\nSimulation started...", flush=True)
 
-    sim = Simulation(filename_with_result="default.txt")
+    sim = Simulation(filename_with_result=FILENAME_WITH_RESULTS)
     sim.run(
         simulation_time=SIMULATION_TIME,
         time_step=TIME_STEP,
@@ -21,3 +23,4 @@ if __name__ == "__main__":
     )
 
     print("Finished", flush=True)
+    print()
